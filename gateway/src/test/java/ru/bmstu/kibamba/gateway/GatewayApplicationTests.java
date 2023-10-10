d060f37b65c9494126bed404add656e83c3377e6
Jacques <jacqueskibamba@gmail.com>
2023-10-10 08:59:06 +0300

    init commit

---
+++ gateway/src/test/java/ru/bmstu/kibamba/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bmstu.kibamba.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ loyalty/src/test/java/ru/bmstu/kibamba/loyalty/LoyaltyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bmstu.kibamba.loyalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
